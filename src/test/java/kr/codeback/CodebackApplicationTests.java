687706478653674a9d523e0949a22456563c8ad1
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-08-20 16:59:19 +0900

    Chore: set up init structure

---
+++ src/test/java/kr/codeback/CodebackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
