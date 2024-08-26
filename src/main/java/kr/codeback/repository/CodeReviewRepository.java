4230735a0a1aa7085936b745e3193f610030ded5
keyy1315 <lovelina1315@gmail.com>
2024-08-26 17:50:14 +0900

    Feat: 게시물 상세 조회 => 좋아요 버튼 개수, 댓글 개수 가져오기

--- src/main/java/kr/codeback/repository/CodeReviewRepository.java
+++ src/main/java/kr/codeback/repository/CodeReviewRepository.java
@@ -1,12 +1,15 @@
 package kr.codeback.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import kr.codeback.model.dto.response.CodeReviewResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import kr.codeback.model.entity.CodeReview;
 
 @Repository
@@ -17,8 +20,13 @@ public interface CodeReviewRepository extends JpaRepository<CodeReview, UUID> {
 		select cr from CodeReview cr
 		join fetch cr.member
 		join fetch cr.codeLanguageCategory
-		join fetch cr.comments
+		left join fetch cr.comments
 		where cr.id = :id
 		""")
+//	댓글이 없을 때도 게시글 조회 -> left join
 	Optional<CodeReview> findById(UUID id);
+
+	Page<CodeReview> findByCodeLanguageCategory_Id(UUID language, Pageable pageable);
+
+	Optional<CodeReview> findByCodeLanguageCategory_Id(UUID language);
 }
