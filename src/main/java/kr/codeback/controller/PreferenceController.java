26bdfbf306087e8f9ee770f88f5850afdfe9eb28
Frod90 <won1680@naver.com>
2024-09-10 10:05:24 +0900

    Refactor: add input preference entityID type

--- src/main/java/kr/codeback/controller/PreferenceController.java
+++ src/main/java/kr/codeback/controller/PreferenceController.java
@@ -1,5 +1,16 @@
 package kr.codeback.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.codeback.model.dto.request.PreferenceRequestDTO;
 import kr.codeback.model.dto.response.PreferenceResponseDTO;
 import kr.codeback.model.entity.Member;
 import kr.codeback.model.entity.Preference;
@@ -7,25 +18,33 @@ import kr.codeback.service.interfaces.MemberService;
 import kr.codeback.service.interfaces.PreferenceService;
 import kr.codeback.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping()
 public class PreferenceController {
 
-    private final PreferenceService preferenceService;
-    private final MemberService memberService;
-    private final JwtUtil jwtUtil;
+	private final PreferenceService preferenceService;
+	private final MemberService memberService;
+	private final JwtUtil jwtUtil;
 
-    @PutMapping("/like/{entityID}")
-    public ResponseEntity<PreferenceResponseDTO> saveOrChange(@PathVariable(name = "entityID") String entityID, @CookieValue(name = "access_token") String accessToken) {
+	@PutMapping("/like/{entityID}")
+	public ResponseEntity<PreferenceResponseDTO> saveOrChange(
+		@PathVariable(name = "entityID") String entityID,
+        @RequestBody PreferenceRequestDTO preferenceRequestDTO,
+        @CookieValue(name = "access_token") String accessToken) {
 
-        String email = jwtUtil.extractEmail(accessToken);
-        Member member = memberService.findByEmail(email);
-        Preference preference = preferenceService.save(member, entityID);
+        System.out.println(preferenceRequestDTO.getType());
 
-        return ResponseEntity.ok().body(PreferenceResponseDTO.builder().entityID(preference.getEntityID()).likeSign(preference.isLike()).build());
-    }
+
+		String email = jwtUtil.extractEmail(accessToken);
+		Member member = memberService.findByEmail(email);
+		Preference preference = preferenceService.save(member, entityID);
+
+		return ResponseEntity.ok()
+			.body(PreferenceResponseDTO.builder()
+				.entityID(preference.getEntityID())
+				.likeSign(preference.isLike())
+				.build());
+	}
 }
