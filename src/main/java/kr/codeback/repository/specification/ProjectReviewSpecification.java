45b24c0953b441c1b4cd5c126abbc985a81f25d2
keyy1315 <lovelina1315@gmail.com>
2024-09-24 16:15:21 +0900

    Fix: 조회기능 쿼리 결과 중복되서 나오는 오류 수정

--- src/main/java/kr/codeback/repository/specification/ProjectReviewSpecification.java
+++ src/main/java/kr/codeback/repository/specification/ProjectReviewSpecification.java
@@ -1,19 +1,26 @@
 package kr.codeback.repository.specification;
 
+import jakarta.persistence.criteria.JoinType;
 import kr.codeback.model.entity.ProjectReview;
 import org.springframework.data.jpa.domain.Specification;
 
 public class ProjectReviewSpecification {
     public static Specification<ProjectReview> hasKeyword(String search) {
         return (root, query, criteriaBuilder) -> {
+            query.distinct(true);
+
             if(search == null || search.isEmpty()) {
                 return criteriaBuilder.conjunction();
             }
+
+            var memberJoin = root.join("member", JoinType.LEFT);
+            var tagJoin = root.join("projectReviewTags", JoinType.LEFT);
+
             return criteriaBuilder.or(
                     criteriaBuilder.like(root.get("title"), "%"+search+"%"),
                     criteriaBuilder.like(root.get("content"), "%" + search + "%"),
-                    criteriaBuilder.like(root.get("member").get("nickname"), "%" + search + "%"),
-                    criteriaBuilder.like(root.get("projectReviewTags").get("tag"), "%" + search + "%")
+                    criteriaBuilder.like(memberJoin.get("nickname"), "%" + search + "%"),
+                    criteriaBuilder.like(tagJoin.get("tag"), "%" + search + "%")
             );
         };
     }
